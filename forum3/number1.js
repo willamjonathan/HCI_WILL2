25d9322fdc44f06f3842be4e18cec394159345db
will <91537837+willamjonathan@users.noreply.github.com>
2021-12-10 11:52:50 +0700

    Create number1.js

---
+++ forum3/number1.js
@@ -0,0 +1,67 @@
+// forum 3 HCI
+//William Jonathan Mulyadi NIM: 2502045683
+// JavaScript Practice 1
+import readlineSync from "readline-sync"
+const blood = readlineSync.question("A+");
+const who = readlineSync.question("B+");
+
+if (blood == "A+"){
+    if ((who == "A+") || (who =="AB+")){
+        console.log(`canGiveBlood("${blood}","${who}") -> true`);
+    }
+    else{
+        console.log(`canGiveBlood("${blood}","${who}") -> false`);
+    }
+}
+else if (blood =="A-"){
+    if((who == "A-") || (who == "A+") || (who =="AB-") || (who =="AB+")){
+        console.log(`canGiveBlood("${blood}","${who}") -> true`);
+    }
+    else{
+        console.log(`canGiveBlood("${blood}","${who}") -> false`);
+    }
+}
+else if (blood == "O+"){
+    if((who == "O+") || (who == "A+") || (who == "B+") || (who == "AB+")){
+        console.log(`canGiveBlood("${blood}","${who}") -> true`);
+    }
+    else{
+        console.log(`canGiveBlood("${blood}","${who}") -> false`);
+    }
+}
+//"O-" can give blood to anyone
+else if (blood == "O-"){
+        console.log(`canGiveBlood("${blood}","${who}") -> true`);
+}
+else if (blood == "B-"){
+    if((who == "B-") || (who == "B+") || (who =="AB-") || (who =="AB+")){
+        console.log(`canGiveBlood("${blood}","${who}") -> true`);
+    }
+    else{
+        console.log(`canGiveBlood("${blood}","${who}") -> false`);
+    }
+}
+else if (blood == "B+"){
+    if((who == "B+") || (who =="AB+")){
+        console.log(`canGiveBlood("${blood}","${who}") -> true`);
+    }
+    else{
+        console.log(`canGiveBlood("${blood}","${who}") -> false`);
+    }
+}
+else if (blood == "AB-"){
+    if((who == "AB-") || (who == "AB+")){
+        console.log(`canGiveBlood("${blood}","${who}") -> true`);
+    }
+    else{
+        console.log(`canGiveBlood("${blood}","${who}") -> false`);
+    }
+}
+else if (blood == "AB+"){
+    if(who == "AB+"){
+        console.log(`canGiveBlood("${blood}","${who}") -> true`);
+    }
+    else{
+        console.log(`canGiveBlood("${blood}","${who}") -> false`);
+    }
+}
