d707a161644109331aaa300468c99552d330f856
will <91537837+willamjonathan@users.noreply.github.com>
2021-12-12 21:02:40 +0700

    Update number2.js

--- forum3/number2.js
+++ forum3/number2.js
@@ -1,1 +1,18 @@
-
+// forum 3 HCI
+//William Jonathan Mulyadi NIM: 2502045683
+// JavaScript Practice 2
+const myfunct = (eMess,key)=>{
+    array = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    c = eMess.length;
+    let a= [];
+    let loop = 0
+    let x = eMess.split("");
+    while ( c > 0){
+        a.push(x[1]);
+        loop++;
+        if (loop = c){
+            stop
+        }
+    }
+}
+//not finished i need more time
