50278e4d36c84e72147991a8f6c022c8cc72aebb
will <91537837+willamjonathan@users.noreply.github.com>
2021-12-13 21:42:31 +0700

    Update number2.js

--- forum3/number2.js
+++ forum3/number2.js
@@ -3,10 +3,17 @@
 // JavaScript Practice 2
 const myfunct = (eMess,key)=>{
     array = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-    c = eMess.length;
+    
     let a= [];
+    let b = key.length;
+    let c = eMess.length;
     let x = eMess.split("");
-        }
+    for i in range (len(0,c)){
+    
+    
     }
+        
+
 }
-//not finished i dont really understand about the task sorry
+//not finished i couldn't find the right solution. 
+//sorry
